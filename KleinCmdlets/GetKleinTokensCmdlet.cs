c25c8860ac4c9c2c0ec5a944b9e0340ae6a05981
mat roberts <mat.roberts@biomni.com>
2016-05-17 08:54:40 +0100

    Added klein cmdlets project

---
+++ KleinCmdlets/GetKleinTokensCmdlet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KleinCmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "KleinTokens")]
+
+    public class GetKleinTokensCmdlet : Cmdlet
+    {
+    }
+}
