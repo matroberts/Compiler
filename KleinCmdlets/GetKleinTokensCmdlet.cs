d57af7326ee9846bfe9bc74ac0c26cd849078b71
mat roberts <mat.roberts@biomni.com>
2016-05-17 20:04:07 +0100

    deleted unwanted file from source control

--- KleinCmdlets/GetKleinTokensCmdlet.cs
+++ KleinCmdlets/GetKleinTokensCmdlet.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
+using KleinCompiler;
 
 namespace KleinCmdlets
 {
     [Cmdlet(VerbsCommon.Get, "KleinTokens")]
-
+    [OutputType(typeof(Token))]
     public class GetKleinTokensCmdlet : Cmdlet
     {
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "Path and name of the klein file to tokenize")]
+        public string Path { get; set; }
     }
 }
