c1fca5ddf7e83327c5025b3955e9194cd1748c2f
mat <mat@moleseyhill.com>
2016-05-26 20:41:05 +0100

    sketched out the parsing alogrithm

--- KleinCompiler/Parser.cs
+++ KleinCompiler/Parser.cs
@@ -1,4 +1,8 @@
-﻿namespace KleinCompiler
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KleinCompiler
 {
     /*
     Klein Grammar
@@ -55,25 +59,106 @@
     */
     public class Parser
     {
-         /*
-          Here is a simple table-driven algorithm. A parser using this algorithm acts on the basis of the current token i in the input stream and the symbol A on top of the stack, until it reaches the end of the input stream, denoted by $.
+        /*
+        The parser acts on the basis of the current token i in the input stream and 
+        the symbol A on top of the stack, until it reaches the end of the input stream, denoted by $.
 
-Push the end of stream symbol, $, onto the stack.
+        Push the end of stream symbol, $, onto the stack.
+        Push the start symbol onto the stack.
 
-Push the start symbol onto the stack.
+        Repeat
+        A is a terminal.
+        If A == i, then
+           Pop A from the stack and consume i.
+        Else we have a token mismatch. 
+           Fail.
 
-Repeat
-A is a terminal.
-If A == i, then
+        A is a non-terminal.
+        If table entry [A, i] contains a rule A := Y1, Y2, ... Yn, then
+           Pop A from the stack. Push Yn, Yn-1, ... Y1 onto the stack, in that order.
+        Else there is no transition for this pair. 
+           Fail.
 
-Pop A from the stack and consume i.
-Else we have a token mismatch. Fail.
-A is a non-terminal.
-If table entry [A, i] contains a rule A := Y1, Y2, ... Yn, then
+        until A == $.
+        */
+        private Stack<Symbol> symbolStack = new Stack<Symbol>();
+        private ParsingTable parsingTable = new ParsingTable();
 
-Pop A from the stack. Push Yn, Yn-1, ... Y1 onto the stack, in that order.
-Else there is no transition for this pair. Fail.
-until A == $.
-*/
+        public bool Parse(Tokenizer tokenizer)
+        {
+            symbolStack.Push(null);
+            symbolStack.Push(new NonTerminalSymbol("Program"));
+
+            Token token = null;
+            while ((token = tokenizer.GetNextToken()) != null)
+            {
+                Symbol symbol = symbolStack.Pop();
+                if (symbol is NonTerminalSymbol)
+                {
+                    var rule = parsingTable.Rule(symbol, token);
+                    if (rule == null)
+                    {
+                        // error
+                    }
+                    else
+                    {
+                        //symbolStack.Push(rule.Reversed());
+                    }
+                }
+                else if (symbol is Token)
+                {
+                    if (symbol == token)
+                    {
+                        // great
+                    }
+                    else
+                    {
+                        // error
+                        return false;
+                    }
+                }
+                else if (symbol == null)
+                {
+                    if (token == null)
+                    {
+                        // great
+                    }
+                    else
+                    {
+                        // fail - program stack is empty, but there are still tokens in the stream
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    public class ParsingTable
+    {
+        public Rule Rule(Symbol nonTerminal, Token token)
+        {
+            return null;
+        } 
+    }
+
+    public class Rule
+    {
+        public List<Symbol> Get() { return null;}
+        public List<Symbol> Reversed() { return null; }
+    }
+
+    public interface Symbol
+    {
+        string Name { get; }
+    }
+
+    public class NonTerminalSymbol : Symbol
+    {
+        public NonTerminalSymbol(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
     }
 }
