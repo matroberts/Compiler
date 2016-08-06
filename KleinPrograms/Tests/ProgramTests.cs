ed86cf3f08cdbc33786909e773ea464e6f897c04
mat <mat@moleseyhill.com>
2016-08-06 13:51:31 +0100

    added tests over the sample parser error programs

--- KleinPrograms/Tests/ProgramTests.cs
+++ KleinPrograms/Tests/ProgramTests.cs
@@ -68,5 +68,25 @@ namespace KleinPrograms.Tests
             Assert.That(error.ErrorType, Is.Not.EqualTo(Error.ErrorTypeEnum.No));
             Assert.That(error.ToString(), Is.EqualTo(message));
         }
+
+        // derive-left-to-right.kln seems to be a valid program
+        [TestCase("euclid-body.kln", "(6, 7) Syntax Error: Attempting to parse symbol 'FuncTail' found token Colon ':'")] 
+        [TestCase("euclid-colon-parms.kln", "(18, 21) Syntax Error: Attempting to parse symbol 'Colon' found token IntegerType 'integer'")] 
+        [TestCase("euclid-comma-call.kln", "(15, 26) Syntax Error: Attempting to parse symbol 'ActualsTail' found token Identifier 'b'")] 
+        [TestCase("euclid-comma-parms.kln", "(4, 23) Syntax Error: Attempting to parse symbol 'FormalTail' found token Identifier 'b'")] 
+        [TestCase("euclid-else-clause.kln", "(16, 8) Syntax Error: Attempting to parse symbol 'FuncTail' found token Colon ':'")] 
+        [TestCase("euclid-parms-type.kln", "(18, 7) Syntax Error: Attempting to parse symbol 'Colon' found token Comma ','")] 
+        [TestCase("euclid-return-type.kln", "(12, 4) Syntax Error: Attempting to parse symbol 'Type' found token If 'if'")] 
+        [TestCase("euclid-then.kln", "(6, 7) Syntax Error: Attempting to parse symbol 'Then' found token Identifier 'a'")] 
+        [TestCase("euclid-then.kln", "(6, 7) Syntax Error: Attempting to parse symbol 'Then' found token Identifier 'a'")] 
+        public void Compiler_ShouldProduceCorrectErrorMessages_ForSampleParserErrors(string filename, string message)
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, @"..\..\..\KleinPrograms\Programs\parser", filename);
+            var input = File.ReadAllText(file);
+            var error = new Compiler().Compile(input);
+
+            Assert.That(error.ErrorType, Is.Not.EqualTo(Error.ErrorTypeEnum.No));
+            Assert.That(error.ToString(), Is.EqualTo(message));
+        }
     }
 }
