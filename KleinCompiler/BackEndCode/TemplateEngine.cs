d6e573bbd43a53ac857672f6c9b8b98813d51157
mat <mat@moleseyhill.com>
2016-10-05 09:03:33 +0100

    Added simple template engine to help with code generation

---
+++ KleinCompiler/BackEndCode/TemplateEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace KleinCompiler.BackEndCode
+{
+    // based on http://stackoverflow.com/a/34679657
+    public class TemplateEngine
+    {
+        private static readonly Regex Regex = new Regex(@"\[(\w*?)]");
+
+        public static string Render(string templateString, Dictionary<string, object> parameters)
+        {
+            return Regex.Replace(templateString, match =>
+            {
+                var param = match.Groups[1].Value;
+                if (parameters.ContainsKey(param) == false)
+                    throw new KeyNotFoundException($"TemplateEngine could not find parameter '{param}' in dictionary");
+                return parameters[param].ToString();
+            });
+        }
+    }
+}
---
+++ KleinCompilerTests/BackEndCode/TemplateEngineTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KleinCompiler.BackEndCode;
+using NUnit.Framework;
+
+namespace KleinCompilerTests.BackEndCode
+{
+    [TestFixture]
+    public class TemplateEngineTests
+    {
+        [Test]
+        public void WithNoReplacementParameters_NothingShouldHappen()
+        {
+            string input = @"This is my text";
+            var parameters = new Dictionary<string, object>();
+
+            var result = TemplateEngine.Render(input, parameters);
+
+            Assert.That(result, Is.EqualTo(input));
+        }
+
+        [Test]
+        public void ReplacementParameters_AreIndicatedWithSquareBrackets()
+        {
+            string input = @"This is [param1] text";
+            var parameters = new Dictionary<string, object>() { ["param1"] = "replacement" };
+
+            var result = TemplateEngine.Render(input, parameters);
+
+            Assert.That(result, Is.EqualTo("This is replacement text"));
+        }
+
+        [Test]
+        public void MultipleReplacementParameters_AreAllowed()
+        {
+            string input = @"This is [param1] [param2]";
+            var parameters = new Dictionary<string, object>()
+            {
+                ["param1"] = "replacement",
+                ["param2"] = "text",
+            };
+
+            var result = TemplateEngine.Render(input, parameters);
+
+            Assert.That(result, Is.EqualTo("This is replacement text"));
+        }
+
+        [Test]
+        public void IfTheDictionary_DoesNotContainTheReplacementParameter_AnExceptionIsThrown()
+        {
+            string input = @"This is [param1] text";
+            var parameters = new Dictionary<string, object>() {  };
+
+            Assert.That(() => TemplateEngine.Render(input, parameters), Throws.Exception.InstanceOf<KeyNotFoundException>().With.Message.EqualTo("TemplateEngine could not find parameter 'param1' in dictionary"));
+        }
+
+
+
+    }
+}
